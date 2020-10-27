a4ba44d27aa41a45c5f345230ad6c540478876cf
jissmon <jisshub>
2020-10-27 18:28:36 +0530

    changes made in post.service.ts file

---
+++ src/app/posts/post.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Post } from './post.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+constructor() { }
+  // create a private property of type Post array 
+  private posts: Post[] = [];
+  getPosts(){
+    // return copy of post array, use spread operator, so changes only affected on its copy, not the original array
+    return [...this.posts];
+  }
+
+  // adding new post
+  addPost(post: Post){
+    this.posts.push(post);
+  }
+
+}  
